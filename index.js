afa629d2cc0bb0831446bd79732e193fb07c5534
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-06-18 13:56:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// // shortGreeting
+// //  contains "Welcome, "
+
+// const shortGreeting = `Welcome,${currentUser}`;
+// 'Edsger Dijkstra'[0];
+
+// 'Edsger Dijkstra'.charAt(0);
+currentUser.slice(0, 1);
+// const currentUser = 'Grace Hopper';
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
